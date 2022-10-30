e184326fca842cfb7e7c1838c298f4b60e21d555
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-30 18:57:59 -0500

    Initial commit

---
+++ people.js
@@ -0,0 +1,15 @@
+const people = [
+  "Zojja",
+  "Hronk",
+  "Eir",
+  "Taimi",
+  "Caithe",
+  "Trahearne",
+  "Scruffy",
+  "Braham",
+  "Aurene",
+];
+
+console.log(people);
+
+module.exports = 'hello';
