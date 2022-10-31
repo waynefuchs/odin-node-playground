6ebfb7e6e9a38e39e21242a9b544c7b6741b6b28
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-10-30 23:18:06 -0500

    Status code chain

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const server = {
 
 app.listen(server.port);
 
+// Add routes
+// Routes are checked one at a time, sequentially until matched
 app.get('/', (req, res) => res.sendFile("./views/index.html", {root: __dirname}));
 app.get('/about', (req, res) => res.sendFile("./views/about.html", {root: __dirname}));
 app.get('/about-us', (req, res) => res.redirect("/about"));
+app.get('/hiddenurl', (req, res) => res.redirect("/"));
+
+// `app.use()` is a catch-all
+// No matches were found, send a 404 page
+// Notice the status() chaining [nice]
+app.use((req, res) => res.status(404).sendFile("./views/404.html", {root: __dirname}));
+
+// This will never get reached, because `app.use` consumes the route
